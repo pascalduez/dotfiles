b9b956331473e1ed26660156cced2dbc91c4749b
Pascal Duez <public@pascalduez.me>
2018-12-29 19:17:42 +0100

    Update install packages

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 command -v pacman >/dev/null 2>&1 && {
     # Archlinux installation
     sudo pacman -Suy
-    sudo pacman -S zsh git tig ack tmux htop wget curl ngrep httpdump nmap traceroute openbsd-netcat vim neovim gnupg tree httpie jq ttf-fira-mono
+    sudo pacman -S zsh git tig ack tmux htop wget curl ngrep httpdump nmap traceroute openbsd-netcat bind-tools vim neovim gnupg tree httpie jq ttf-fira-mono
 }
 
 command -v apt-get >/dev/null 2>&1 && {
